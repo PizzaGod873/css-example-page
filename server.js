b7e1e7c0ee928c6bbb26f96bf8286ed6d5ef16e5
Andrew <150487080+PizzaGod873@users.noreply.github.com>
2025-07-22 11:41:18 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,48 +6,24 @@ app.set("trust proxy", true);
 
 app.get("/", (req, res) => {
   const ip = req.ip;
-  const ua = req.get("User-Agent") || "";
-  console.log(`Visitor IP: ${ip}, User-Agent: ${ua}`);
-  fs.appendFileSync("ips.txt", `${ip} | ${ua}\n`);
+  console.log("Visitor IP:", ip);
+  fs.appendFileSync("ips.txt", ip + "\n");
 
-  // Check if it's a bot (Discord, Twitter, Facebook, etc.)
-  const isBot = /discord|bot|twitter|facebook|embed/i.test(ua);
-
-  if (isBot) {
-    // Send fake preview page to trick bots
-    res.send(`
-      <!DOCTYPE html>
-      <html>
-        <head>
-          <title>My Cool Project</title>
-          <meta property="og:title" content="CSS test">
-          <meta property="og:description" content="pretty cool CSS stuffs">
-        </head>
-        <body>
-          Nothing to see here.
-        </body>
-      </html>
-    `);
-  } else {
-    // Real user: Rickroll!
-    res.send(`
-      <!DOCTYPE html>
-      <html>
-        <head>
-          <meta charset="UTF-8">
-          <title>Loading...</title>
-          <script>
-            setTimeout(() => {
-              window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-            }, 1000);
-          </script>
-        </head>
-        <body>
-          <p>Loading...</p>
-        </body>
-      </html>
-    `);
-  }
+  res.send(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Loading...</title>
+      </head>
+      <body>
+        <p>Loading...</p>
+        <script>
+          // Attempt to open Rickroll in a new tab on page load
+          window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank');
+        </script>
+      </body>
+    </html>
+  `);
 });
 
 const port = process.env.PORT || 3000;
