0272e4e26c746fd1ad7ffd5d85f4643b90d0e6d8
Andrew <150487080+PizzaGod873@users.noreply.github.com>
2025-07-22 11:44:28 -0500

    Update server.js

--- server.js
+++ server.js
@@ -9,21 +9,8 @@ app.get("/", (req, res) => {
   console.log("Visitor IP:", ip);
   fs.appendFileSync("ips.txt", ip + "\n");
 
-  res.send(`
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <title>Loading...</title>
-      </head>
-      <body>
-        <p>Loading...</p>
-        <script>
-          // Attempt to open Rickroll in a new tab on page load
-          window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank');
-        </script>
-      </body>
-    </html>
-  `);
+  // Immediately redirect with HTTP 302
+  res.redirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
 });
 
 const port = process.env.PORT || 3000;
